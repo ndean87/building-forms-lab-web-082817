3a5317fa1ccbc92a59a3ce85637a82a7272bd7a9
ndean87 <ndean87@gmail.com>
2017-11-18 16:08:23 -0500

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 
 class Bands extends Component {
+
+
   render(){
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return <li key={index}>{band}</li>
+    });
     return (
-      <div>
-        band input
-      </div>
+      <ul>
+        {bands}
+      </ul>
     );
   }
 };
 
 export default Bands;
+
+
+// You will need to write the Bands component. This component should display a ul with a list element for each of the bands in the store's state. Each list element should have text with the corresponding name of the band.
+// You will also need to fill in the manageBand reducer located under ./src/reducers/manageBand.js. It will need to respond to an action of type 'ADD_BAND' and correspondingly concatenate a new band each time the action is dispatched.
